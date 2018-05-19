a31a6992d93755dc8a9e01f135aab5026e691c73
Artem <Artem@danils-macbook-pro.local>
2018-05-19 18:03:53 +0300

    Initial Commit

---
+++ FastfoodApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastfoodApp
+//
+//  Created by Artem on 5/19/18.
+//  Copyright © 2018 major-food. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
